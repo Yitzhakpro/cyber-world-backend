c724e3ba70566721007c9984b3a8a905efe1291d
Yitzhak <isako14@gmail.com>
2022-05-27 14:14:40 +0300

    added @config module resolution

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import fastifyCookie from '@fastify/cookie';
 import fastifyJwt from '@fastify/jwt';
 import { dbConnections } from './plugins';
 import rootRouter from './routes';
-import config from './config';
+import config from '@config';
 
 const corsConfig = config.get('cors');
 const accessTokenSecret = config.get('auth.jwt.secret');
