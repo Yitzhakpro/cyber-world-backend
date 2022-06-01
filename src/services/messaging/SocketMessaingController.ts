01a7fff9e5e24625fa68fad2fba2e402fe517d5f
Yitzhak <isako14@gmail.com>
2022-06-01 14:44:52 +0300

    added join room event & check for already in room

--- src/services/messaging/SocketMessaingController.ts
+++ src/services/messaging/SocketMessaingController.ts
@@ -1,4 +1,4 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { JWT } from '@fastify/jwt';
 import Cookie from 'cookie';
@@ -55,6 +55,19 @@ export default class SocketMessaingController {
         this.socketServer.on('connection', (socket) => {
             console.log(`[${socket.data.rank}]${socket.data.username} connected`);
 
+            socket.on('join_room', (roomID: string) => {
+                const alreadyInRoom = this.checkIfAlreadyInRoom(socket);
+
+                if (alreadyInRoom) {
+                    console.log(`[!] '${socket.data.username}' can't join room: ${roomID} because he is already in a room`);
+                    socket.emit('join_failed', 'you already in room, try to refresh your browser');
+                } else {
+                    socket.join(roomID);
+                    console.log(`[v] '${socket.data.username}' joined room: ${roomID}`);
+                    socket.emit('joined_successfully');
+                }
+            });
+
             socket.on('message', (message: string) => {
                 console.log(`${socket.data.username} sent: ${message}`);
             });
@@ -63,5 +76,13 @@ export default class SocketMessaingController {
                 console.log(`[${socket.data.rank}]${socket.data.username} disconncted, reason: ${reason}`);
             });
         });
+    }
+
+    checkIfAlreadyInRoom(socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, SocketUserData>): boolean {
+        if (socket.rooms.size > 1) {
+            return true;
+        }
+
+        return false;
     }
 }
